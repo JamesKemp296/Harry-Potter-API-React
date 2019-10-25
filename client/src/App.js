8da0905a300c1ecb647995d8d0dac255d43b9bf1
James Kemp <jameskemp2796@yahoo.com>
2019-10-25 13:32:35 -0400

    Basic react routs are made

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import SortMe from './pages/SortMe'
+import CohortList from './pages/CohortList'
+import CohortStudent from './pages/CohortStudent'
+import Navbar from './components/Navbar'
 
-App = () => (
-  <CharacterList/>
+const App = () => (
+  <BrowserRouter>
+    <Navbar/>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/sort" component={SortMe} />
+      <Route exact path="/cohort" component={CohortList} />
+      <Route path="/cohort/:id" component={CohortStudent} />
+    </Switch>
+  </BrowserRouter>
 )
 
-export default App;
+export default App
